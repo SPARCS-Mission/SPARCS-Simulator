7d01178af25cfd99c3254fb0fd70c27aa4358a35
Danial Rezaei <drezaei634@gmail.com>
2024-12-11 10:55:26 +0330

    Update ADCS MIL
    - Reviewed and debugged all blocks related to Environmental and onboard models and hardware.
    - Debugged the detumbling(B-dot) algorithm.

--- MIL/SPARCS_MIL_init.m
+++ MIL/SPARCS_MIL_init.m
@@ -9,44 +9,57 @@ epoch.Minute = 24;
 epoch.Second = 45;
 epoch.DecimalYear = decyear(epoch.Year, epoch.Month, epoch.Day, epoch.Hour, epoch.Minute, epoch.Second);
 %% Orbital Elements
-Semi_major_axis = 6921000;     % (m)
-Inclination = 55;              % (deg)
-RAAN = 0;                      % (deg)
-Argument_of_latitude = 0;      % (deg)
-simulator.EarthGConst = 3.986004418e14; % Earth's standard gravitational parameter in m^3/s^2
+Semi_major_axis = 6921000;                    % (m)
+Inclination = 55;                             % (deg)
+RAAN = 45;                                    % (deg)
+Argument_of_latitude = 45;                    % (deg)
+simulator.EarthGConst = 3.986004418e14;       % Earth's standard gravitational parameter in m^3/s^2
 %% Attitude Kinematics & Dynamics
 % Initial Euler Angles (rad)
-attitude.Phi0 = 12 * pi /180;
-attitude.Theta0 = -15 * pi /180;
-attitude.Psi0 = 18 * pi /180;
+rng(1);
+EulerUnit = randn(3,1); 
+EulerUnit = EulerUnit ./ norm(EulerUnit);
+EulerNorm = 90;                               %deg
+Euler_Angle = deg2rad(EulerNorm).*EulerUnit;
+attitude.Phi0 = Euler_Angle(1);
+attitude.Theta0 = Euler_Angle(2);
+attitude.Psi0 = Euler_Angle(3);
+
 % Initial Omega in Body (rad/s)
-attitude.P0 = 0.1;
-attitude.Q0 = 0.15;
-attitude.R0 = 0.2;
+rng(1);
+OmegaUnit = randn(3,1);
+OmegaUnit = OmegaUnit./norm(OmegaUnit);
+OmegaNorm = 0;                               %deg/s
+Omega = deg2rad(OmegaNorm).*OmegaUnit;
+attitude.P0 = Omega(1);
+attitude.Q0 = Omega(2);
+attitude.R0 = Omega(3);
+
 % Moment of Inertia
-satellite.MOI = [0.0393 -0.0044 -0.0100; -0.0044 0.0397 0.0096; -0.0100 0.0096 0.0118];
+satellite.MOIJ = [0.0393 -0.0044 -0.0100; -0.0044 0.0397 0.0096; -0.0100 0.0096 0.0118];
+satellite.MOI = diag(diag(satellite.MOIJ));
 satellite.InvMOI = inv(satellite.MOI);
 %% Simulation
 visSampleTime = 0.1;
 %% Magnetoqures Model
-satellite.MTQ.DPMoment = 0.5; % Nominal Dipole
+satellite.MTQ.DPMoment = 0.2;                 % Nominal Dipole
 %% Power
-satellite.MTQ.Power = [1.5 1.5 1.5]; % W
-satellite.Gyro.NominalVoltage = 3.3;
-satellite.Gyro.NominalPower = 0.5;
-satellite.MM.NominalPower = 0.4;
-satellite.MM.NominalVoltage = 3.3;
+satellite.MTQ.Power = [1.5 1.5 1.5];          % W
+satellite.Gyro.NominalVoltage = 3.3;          % V
+satellite.Gyro.NominalPower = 0.5;            % W
+satellite.MM.NominalPower = 0.4;              % W
+satellite.MM.NominalVoltage = 3.3;            % W
 
 %% Sensor Specifications
 
 % GPS Specs
-satellite.GPS.Frequency = 1;                  %Hz
+satellite.GPS.Frequency = 1;                  % Hz
 satellite.GPS.Position.Bias = 5;              % m
-satellite.GPS.Position.NoiseSTDDEV = 2.5;     %m
-satellite.GPS.Velocity.Bias = 0.1;            %m/s
+satellite.GPS.Position.NoiseSTDDEV = 2.5;     % m
+satellite.GPS.Velocity.Bias = 0.1;            % m/s
 
 % Gyro Specs
-satellite.Gyro.Frequency = 1;                 %1Hz
+satellite.Gyro.Frequency = 1;                 % 1Hz
 satellite.Gyro.ScaleFactor = 0.5;             % precent (%)
 satellite.Gyro.Range = [-150, 150] ;          % deg/s
 satellite.Gyro.Bias = 7;                      % deg/hr
@@ -55,7 +68,7 @@ satellite.Gyro.RateRandomWalk = 6;            % deg/sqrt(hr^3)
 satellite.Gyro.BiasInstability = 0.5;         % deg/s
 
 % Magnetometer (MM) Specs
-satellite.MM.Frequency = 1;                   %Hz
+satellite.MM.Frequency = 1;                   % Hz
 satellite.MM.Accuracy = 1;                    % precent (%)
 satellite.MM.Sensitivity = 1;                 % mV/nT
 satellite.MM.Range = [-1, 1].*2e5;            % nT
